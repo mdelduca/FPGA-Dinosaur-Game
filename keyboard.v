e10f1fba66f8e4ad3cc6e22753158699a100febc
Michael <76981570+mdelduca@users.noreply.github.com>
2023-11-21 12:44:14 -0500

    Keyboard edits

--- keyboard.v
+++ keyboard.v
@@ -1,13 +1,18 @@
 
 module keyboard(
 	input wire clk,
-	input wire PS2_clk,
-	input wire PS2_data,
+	input wire PS2C,
+	input wire PS2D,
 	input wire[10:0] data,
+	input start,
 	input parity,
 	input stop
 	);
 	
+	reg[7:0] currentData, prevData;
+	reg[3:0] counter;
+
+	always@ (negedge PS2C)
 
 
 
