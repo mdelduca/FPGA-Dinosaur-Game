08b85f3eb12ca322a3c7f630bbede9fde290cd48
Michael <76981570+mdelduca@users.noreply.github.com>
2023-11-21 23:56:57 -0500

    Keyboard only scans for input when key is pressed, added pin assignment file

--- keyboard.v
+++ keyboard.v
@@ -1,24 +1,24 @@
 
 module keyboard(
-	input wire clk,
-	input wire PS2C,
-	input wire PS2D,
-	input start,
-	input parity,
-	input stop,
+	input wire PS2_CLK,
+	input wire PS2_DAT,
 	input resetN,
 
-	output reg[7:0] heldData, pressedData,
-	output reg[10:0] data
+	output start,
+	output parity,
+	output stop,
+	output reg keyReleased,
+	output reg[10:0] data,
+	output reg[7:0] heldData, pressedData
 	);
 	
-	reg[7:0] currentData, prevData, heldData, pressedData;
+	reg[3:0] oneCount;
+	reg[7:0] currentData, prevData;
 	reg[3:0] counter;
 	reg error;
-	reg keyReleased;
+	reg flag;
 
 	assign start = data[0];
-	assign currentData = data[8:1];
 	assign parity = data[9];
 	assign stop = data[10];
 
@@ -27,54 +27,43 @@ module keyboard(
 		currentData <= 8'b0;
 		prevData <= 8'b0;
 		data <= 11'b0;
-		start <= 0;
-		parity <= 0;
-		stop <= 0;
-		resetN <= 0;
-		error <= 0
+		oneCount <= 0;
 	end
 
-	always@ (negedge PS2C) begin
-
-		resetN <= 0;
-
+	always@ (negedge PS2_CLK) begin
+		error <= 0;
+		if (start == 1'b1) begin
+			error <= 1'b1;
+		end
 		if (counter < 11) begin
-			data[counter] <= PS2D;
+			oneCount <= oneCount + PS2_DAT;
+			data[counter] <= PS2_DAT;
+			currentData <= data[8:1];
 			counter <= counter + 1;
 		end
-		else counter <= 0;
-
-		// case(counter)
-		// 	1'b0000: data[0] <= PS2D;
-		// 	1'b0001: data[1] <= PS2D;
-		// 	1'b0010: data[2] <= PS2D;
-		// 	1'b0011: data[3] <= PS2D;
-		// 	1'b0100: data[4] <= PS2D;
-		// 	1'b0101: data[5] <= PS2D;
-		// 	1'b0110: data[6] <= PS2D;
-		// 	1'b0111: data[7] <= PS2D;
-		// 	1'b1000: data[8] <= PS2D;
-		// 	1'b1001: data[9] <= PS2D;
-		// 	1'b1010: data[10] <= PS2D;
-		// endcase
+		else begin 
+			counter <= 0;
+			data <= 0;
+			oneCount <= 0;
+		end
 
 		if (resetN || error) begin
 			counter <= 0;
 			currentData <= 8'b0;
+			oneCount <= 0;
 			if (resetN) begin
 				prevData <= 8'b0;
 				heldData <= 8'b0;
 				pressedData <= 8'b0;
 			end
 			data <= 11'b0;
-			error <= 0;
 			keyReleased <= 0;
 		end
 	end
 
 	always@(posedge stop) begin
-		if (start != 0 || parity ^ currentData[0]) begin
-			error = 1'b1;
+		if (start != 0 || parity==oneCount[0] || currentData == 0 || currentData == 8'hFF) begin
+			error <= 1'b1;
 		end
 		else begin
 			if (prevData == 8'hF0) begin
@@ -111,11 +100,24 @@ module keyboard(
 		end
 	end
 
-	// always@(posedge keyReleased) begin
+	always@(posedge keyReleased) begin
 		
-	// end
+	end
 
 
 
 
+endmodule
+
+module keyController
+(
+	input wire PS2_CLK,
+	input wire PS2_DAT,
+	input resetN,
+	input keyReleased,
+	input[7:0] heldData, pressedData,
+
+	output reg jump, pause
+);
+	
 endmodule
