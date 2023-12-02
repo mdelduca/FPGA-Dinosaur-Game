5e529e70e90af2760b9aa9c083b9a9b8197bc0ad
Michael <76981570+mdelduca@users.noreply.github.com>
2023-12-02 15:17:14 -0500

    Changed Frequency

--- FSM.v
+++ FSM.v
@@ -299,7 +299,7 @@ module KBDatapath #(
 					jumpTimer <= jumpTimer - 1;
 				end
 				else begin
-					jumpTimer <= 21'd1199999;
+					jumpTimer <= 21'833333;
 					height <= nextHeight;
 					nextHeight <= nextHeight - (velocity*counter);
 				end
